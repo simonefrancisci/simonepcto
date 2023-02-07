63287991fd9bd05bff24ad51c4fcb0f71df8158f
simonefrancisci <124669324+simonefrancisci@users.noreply.github.com>
2023-02-07 11:01:29 +0100

    Add files via upload

--- treno.c
+++ treno.c
@@ -2,9 +2,15 @@
 
 int main()
 {
+    int a = 17;
+    int b = 3;
+    int divisione = a / b;
+    int resto = a % b;
+
+    printf("la divisione tra a e b è: %d\n", divisione);
+  
+   
+   
 
 
-
-
-    
 }
