8947b34364b221858dab4f00a807073736036ece
simonefrancisci <124669324+simonefrancisci@users.noreply.github.com>
2023-02-08 10:19:10 +0100

    Add files via upload

---
+++ termini.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+int b;
+int c;
+
+printf("inserisci tre numeri\n");
+scanf("%d %d %d", &a,&b,&c);
+ 
+if(a+b>c && b+c>a && c+a>b)
+{
+    printf("è un triangolo\n");
+}
+
+else
+{
+    printf("non è un triangolo\n");
+}
+
+
+
+
+
+}
