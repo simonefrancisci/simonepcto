7c4fe2a3290cc17801a4d6d473a56de09c1626bb
simonefrancisci <124669324+simonefrancisci@users.noreply.github.com>
2023-02-08 09:53:16 +0100

    Add files via upload

--- binari.c
+++ binari.c
@@ -2,3 +2,18 @@
 
 int main()
 {
+    int anno;
+    printf("inserisci un numero\n");
+    scanf("%d", &anno);
+
+    if((anno % 4==0 && anno%100!=0) || anno%400==0)
+    {
+        printf("bisestile\n");
+    }
+
+    else
+    {
+        printf("non bisestile\n");
+    }
+
+}
